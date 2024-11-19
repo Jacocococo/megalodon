48d93dd8b2f11b719d777d20beab5c8b1a74874e
Jacoco <63549689+Jacocococo@users.noreply.github.com>
2024-11-19 14:55:12 +0100

    Fix shrimped auth redirect

--- mastodon/src/main/java/org/joinmastodon/android/api/session/AccountSessionManager.java
+++ mastodon/src/main/java/org/joinmastodon/android/api/session/AccountSessionManager.java
@@ -82,10 +82,12 @@ public class AccountSessionManager{
 
 	public static String getRedirectURI() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("moshidon-android-");
 		if (BuildConfig.BUILD_TYPE.equals("debug") || BuildConfig.BUILD_TYPE.equals("nightly")) {
+			builder.append("moshidon-android-");
 			builder.append(BuildConfig.BUILD_TYPE);
 			builder.append('-');
+		} else {
+			builder.append("moshidon-shrimped-android-");
 		}
 		builder.append("auth://callback");
 		return builder.toString();
